9ebbfbcc27b16b52db7ecf062dffb1a24caf62f2
“minica-cai” <minica975@gmail.com>
2024-03-31 13:14:43 +0800

    feat:添加symbol，decimals，totalSupply的单元测试

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -1,5 +1,13 @@
 // SPDX-License-Identifier: MIT 
 pragma solidity >=0.4.22 <0.9.0;
 contract Token {
-    string public name = 'My Name';
+    string public name = 'DApp mi';
+    string public symbol = 'DApp';
+    uint256 public decimals = 18; //以太坊的小数位可以被18位
+    uint256 public totalSupply;
+
+    // 总的供应量 = 代币 * decimals
+    constructor() public {
+        totalSupply = 1000000 * (10 ** decimals) ;
+    }
 }
--- test/Token.test.js
+++ test/Token.test.js
@@ -6,13 +6,39 @@ require('chai')
     .should()
 
 contract('Token',(accounts)=>{
-    describe('deployment',()=>{
+    // 声明相关的使用的变量
+    let token;
+    const name = 'DApp mi' ;// 代码注意规范化
+    const symbol = 'DApp';
+    const decimals = '18';
+    // 这里为什么用字符串而不是数字的原因：如果是数字的话，数据太大js不好验证
+    const totalSupply = '1000000000000000000000000';
+
+    beforeEach(async ()=>{
+        // 获取代币
+        token = await Token.new()
+    })
+    describe('deployment',async ()=>{
         it('tracks the name',async ()=>{
-            // 获取代币
-            const token = await Token.new()
             const result = await token.name()
-            result.should.equal('My Name');
+            result.should.equal(name);
             // read token name
         })
+
+        it('tracks the symbol',async ()=>{
+            const result = await token.symbol()
+            result.should.equal(symbol)
+        })
+
+        it('tracks the decimals',async ()=>{
+            const result = await token.decimals()
+            result.toString().should.equal(decimals)
+        })
+
+        it('tracks the total supply',async ()=>{
+            const result = await token.totalSupply()
+            result.toString().should.equal(totalSupply)
+        })
+
     })
 })
