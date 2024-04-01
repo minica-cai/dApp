f8777381a32bbe18cbd8145df5a3f953ed3e3c10
“minica-cai” <minica975@gmail.com>
2024-04-01 14:06:07 +0800

    feat:委托allowance的转移，首先要获得批准，然后才能进行转移，需要判断转移条件，添加对应的单元测试

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -17,12 +17,14 @@ contract Token {
     // track balances 获取余额
     // mapping 映射关联键值对 address账户地址 uint256余额单位，1字节等于8个比特，而32指的就是32个字节，即8*32=256比特
     mapping(address => uint256) public balanceOf; 
+    mapping(address => mapping (address => uint256)) public allowance; //双重映射 allowance交易所批准的金额，转移的金额数量
     // send tokens 发送tokens
 
 
     // Events
     // 转移事件，
-    event Transfer(address indexed from,address indexed to,uint256 value);
+    event Transfer(address indexed from,address indexed to, uint256 value);
+    event Approve(address indexed owner, address indexed spender, uint256 value);
 
     // 总的供应量 = 代币 * (10 ** decimals) 
     constructor() public {
@@ -32,19 +34,41 @@ contract Token {
     }
 
     // 转移函数
-    function transfer (address _to, uint256 _value) public returns(bool sucess) {
+    function transfer (address _to, uint256 _value) public returns(bool success) {
+        require(balanceOf[msg.sender] >= _value); // 判断是否有足够的余额进行转移
+        _transfer(msg.sender , _to , _value);
+        return true;
+    }
+
+    //_开头的内部函数 internal 关键字为内部函数的关键字，
+    function _transfer(address _from,address _to, uint256 _value ) internal {
         // solidity 中require(true)向下执行代码，require(false)停止执行
         require(_to != address(0));
-        require(balanceOf[msg.sender] >= _value); // 判断是否有足够的余额进行转移
-        
         // 转移者的余额 = 余额 - _value
         // 接受者的余额 = 余额 + _value
-        balanceOf[msg.sender] = balanceOf[msg.sender].sub(_value);
+        balanceOf[_from] = balanceOf[_from].sub(_value);
         balanceOf[_to] = balanceOf[_to].add(_value);
+         // 触发订阅的事件
+        emit Transfer(_from, _to, _value);
+    }
 
-        // 触发订阅的事件
-        emit Transfer(msg.sender, _to, _value);
+    // 批准函数
+    function approve(address _spender, uint256 _value) public returns (bool success){
+        require(_spender != address(0)); // 无效地址
+        allowance[msg.sender][_spender] = _value;
+        emit Approve(msg.sender, _spender, _value);
+        return true;
+    }
 
+    // `transfer` 用于直接的地址间转移，
+    // 而 `transferFrom` 用于授权操作者代表拥有者进行转移。在调用 `transferFrom` 之前，通常需要拥有者通过调用 `approve` 函数提前授权。
+    function transferFrom(address _from, address _to, uint256 _value) public returns(bool success){
+        // 添加函数限制条件
+        require(_value <= balanceOf[_from]); // 批准的数量 <= 来源者的资金
+        require(_value <= allowance[_from][msg.sender]); // 批准的数量 <= 交易所的余额
+        // 余额 = 余额 - _value
+        allowance[_from][msg.sender] =  allowance[_from][msg.sender].sub(_value);
+        _transfer(_from , _to , _value);
         return true;
     }
 }
--- test/Token.test.js
+++ test/Token.test.js
@@ -7,7 +7,8 @@ require('chai')
     .use(require('chai-as-promised'))
     .should()
 
-contract('Token',([deployer,receiver])=>{
+// 测试文件的描述需要进行合理的嵌套，方便阅读信息
+contract('Token',([deployer,receiver,exchange])=>{
     // 声明相关的使用的变量
     let token;
     const name = 'DApp mi' ;// 代码注意规范化
@@ -103,6 +104,105 @@ contract('Token',([deployer,receiver])=>{
             it('rejects invalid recipients',async ()=>{
                 //  认为最后一个地址是0x0
                 await token.transfer(0x0, amount, {from : deployer}).should.be.rejected;
+            })
+        })
+    })
+
+    //允许别人使用token
+    describe('approving tokens',async ()=> {
+        let result;
+        let amount;
+
+        beforeEach(async ()=>{
+            amount = tokens(100)
+            result = await token.approve(exchange, amount, { from : deployer})
+        })
+
+        // 成功
+        describe('sucess', async () => {
+            // 余额验证
+            it('allocates an allowance for delegated token spending on exchange', async ()=> {
+                const allowance = await token.allowance(deployer,exchange)
+                allowance.toString().should.equal(amount.toString())
+            })
+            // 事件触发 
+            it('emits a Approve event', async () => {
+                const log = result.logs[0]
+                log.event.toString().should.be.eq('Approve') // 验证事件名
+                // 验证提供者，消费者，批准数量
+                const event = log.args
+                event.owner.toString().should.equal(deployer,'owner is correct')
+                event.spender.toString().should.equal(exchange,'spender is correct')
+                event.value.toString().should.equal(amount.toString(),'value is correct')
+            });
+
+        });
+        describe('failure',async () => {
+            it('rejects invalid recipients', async () => {
+                //  认为最后一个地址是0x0
+                await token.transfer(0x0, amount, {from : deployer}).should.be.rejected;
+            });
+        });
+    })
+
+    // 发送代币的单元测试
+    describe('delegated token transfers',()=>{
+        let result;
+        let amount;
+
+        beforeEach(async ()=>{
+            amount = tokens(100)
+            // 等待执行批准,也就是在代币转移之前，是需要先从发布者那里批准代币到交易所
+            await token.approve(exchange, amount, { from : deployer})
+
+        })
+
+        // 转移成功的单元测试
+        describe('sucess', async () => {
+            beforeEach(async ()=>{
+                // 等待执行转移函数
+                result = await token.transferFrom(deployer , receiver , amount ,{from : exchange})
+            })
+            // 发送代币需要注意验证的点，转移前分别的余额，执行转移，转移后分别的余额
+            it('transfer token balances',async ()=>{
+                let balanceOf;
+                // 转移之后
+                balanceOf = await token.balanceOf(deployer)
+                balanceOf.toString().should.equal(tokens(999900).toString()) // 发送着的余额
+                balanceOf = await token.balanceOf(receiver) // 接受者的余额
+                balanceOf.toString().should.equal(tokens(100).toString())
+    
+            })
+            // 验证余额
+            it('resets the allowance', async ()=> {
+                const allowance = await token.allowance(deployer,exchange)
+                allowance.toString().should.equal('0')
+            })
+            // 触发转移事件的单元测试
+            it('emits a Transfer event',async ()=>{
+                const log = result.logs[0]
+                log.event.toString().should.eq('Transfer')
+                const event = log.args
+                event.from.toString().should.equal(deployer,'from is correct')
+                event.to.toString().should.equal(receiver,'to is correct')
+                event.value.toString().should.equal(amount.toString(),'value is correct')
+            })            
+        });
+
+        // 转移失败的单元测试
+        describe('failure',async ()=>{
+            // 试图转移的代币数量超过批准的数量
+            it('rejects insufficient amount',async ()=> {
+                let invalidAmount;
+                //当转移的数量 > 代币总量的时候 --- 结果是要被拒绝
+                invalidAmount = tokens(10000000)
+                await token.transferFrom(deployer ,receiver, invalidAmount, {from : deployer}).should.be.rejectedWith(EVM_REVERT)
+            })
+
+            // 当地址错误，也就是无效的接受者
+            it('rejects invalid recipients',async ()=>{
+                //  认为最后一个地址是0x0
+                await token.transferFrom(deployer ,0x0, amount, {from : deployer}).should.be.rejected;
             })
         })
     })
